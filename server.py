fc7249b3233985603b78b2a8d7f8c00ff526d3e1
taichingkan <taichingkan@gmail.com>
2024-04-08 17:02:28 +0800

    Added 'Edit Profile (Patient)' functionality

--- server.py
+++ server.py
@@ -15,48 +15,48 @@ class BlockPoW:
         self.previous_hash = previous_hash
         self.nonce = 0  # 初始化nonce值
         self.hash = self.calculate_hash()
-        
+
     def calculate_hash(self):
         block_string = "{}{}{}{}{}".format(self.index, self.transactions, self.timestamp, self.previous_hash, self.nonce)
         return hashlib.sha256(block_string.encode()).hexdigest()
-    
-    
+
+
 
 class BlockchainPoW:
     def __init__(self):
         self.chain = []
         self.create_genesis_block()
-        
+
     def create_genesis_block(self):
         genesis_block = BlockPoW(0, "Genesis Block", time.time(), "0")
         self.mine(genesis_block)
         self.chain.append(genesis_block)
-        
+
     def add_block(self, transactions):
         previous_block = self.chain[-1]
         new_block = BlockPoW(len(self.chain), transactions, time.time(), previous_block.hash)
         self.mine(new_block)
         self.chain.append(new_block)
-        
+
     def mine(self, block, difficulty=5):
         challenge = "0" * difficulty
         while block.hash[:difficulty] != challenge:
             block.nonce += 1
             block.hash = block.calculate_hash()
-        
+
     def is_valid(self):
         for i in range(1, len(self.chain)):
             current_block = self.chain[i]
             previous_block = self.chain[i-1]
-            
+
             if current_block.hash != current_block.calculate_hash():
                 return False
-            
+
             if current_block.previous_hash != previous_block.hash:
                 return False
-            
+
         return True
-    
+
 bc = BlockchainPoW()
 
 @app.route('/login', methods=['POST'])
@@ -84,6 +84,17 @@ def upload_record():
     bc.add_block(str(diagnosis))
     # 写入csv
     return jsonify(doctor_key=doctor_key)
+
+
+@app.route('/edit_profile', methods=['POST'])
+def edit_profile():
+    patient_private_key_upload = request.form.get('patient_private_key_upload')
+    patient_public_key_upload = request.form.get('patient_public_key_upload')
+    bc.add_block("Edit patient")
+    # 写入csv
+    return jsonify(patient_private_key=patient_private_key_upload, patient_public_key=patient_public_key_upload)
+
+
 
 # 用于Retrieve信息
 @app.route('/access_record', methods=['GET'])
