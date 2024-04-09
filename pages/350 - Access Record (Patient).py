76e49cf1377f405b482fb5de2d69ad4a86917055
taichingkan <taichingkan@gmail.com>
2024-04-09 22:55:57 +0800

    (Not Functional yet) Make checkbox for medical record sharing

--- pages/350 - Access Record (Patient).py
+++ pages/350 - Access Record (Patient).py
@@ -20,6 +20,9 @@ def access_record(patient_private_key):
         st.error("Failed to access records.")
         return []
 
+def onChangeSharing(edited_df):
+    print("Do stuff")
+
 # UI
 if 'patient_private_key' in st.session_state:
     st.markdown(f"""<p style="font-size: 16px; text-align: right;">Logged in as Patient {st.session_state['patient_private_key']}</p>""", unsafe_allow_html=True)
@@ -43,16 +46,41 @@ if 'patient_private_key' in st.session_state:
             for record in records:
                 if record['private_key'] == st.session_state['patient_private_key']:
                     recordsList[i][0] = record['private_key']
-                    recordsList[i][1] = record['diagnosis']
-                    recordsList[i][3] = record['sharing']
+                    recordsList[i][2] = record['diagnosis']
+                    recordsList[i][3] = (record['sharing'] == "Public") # True only if Sharing is set to Public
 
                     # 时间戳只取年月日时分
-                    recordsList[i][2] = datetime.strptime(record['timestamp'], '%Y-%m-%d %H:%M:%S.%f').strftime('%Y-%m-%d %H:%M')
+                    recordsList[i][1] = datetime.strptime(record['timestamp'], '%Y-%m-%d %H:%M:%S.%f').strftime('%Y-%m-%d %H:%M')
                     i += 1
 
             recordsArray = np.array(recordsList)
+            print(recordsArray)
 
-            df = pd.DataFrame(data=recordsArray[:, 1:4], columns=("Diagnosis", "Date & Time", "Sharing"))
-            st.dataframe(df, hide_index=True)
+            df = pd.DataFrame(data=recordsArray[:, 1:4], columns=("Date & Time", "Diagnosis", "Sharing"))
+
+            if "df_value" not in st.session_state:
+                st.session_state.df_value = df
+
+            edited_df = st.data_editor(
+                df,
+                #key=f"editor",
+                hide_index=True,
+                column_config={
+                    "Sharing": st.column_config.CheckboxColumn(
+                        help="Check the boxes next to the medical records if you wish to allow doctors to view them.",
+                        label="Sharing"
+                    )
+                },
+            )
+
+            if edited_df is not None and not edited_df.equals(st.session_state["df_value"]):
+                # This will only run if
+                # 1. Some widget has been changed (including the dataframe editor), triggering a
+                # script rerun, and
+                # 2. The new dataframe value is different from the old value
+                onChangeSharing(edited_df)
+                st.session_state["df_value"] = edited_df
+
+
 else:
     st.warning("You must be logged in to view this page.")
